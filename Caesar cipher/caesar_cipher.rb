cec079de6135f41e512cff9f2dca332bf726ff07
PBalazs0517 <peter.balazs0517@gmail.com>
2022-08-08 00:00:50 +0200

    deleted some comments from caesar_cipher.rb

--- Caesar cipher/caesar_cipher.rb
+++ Caesar cipher/caesar_cipher.rb
@@ -5,9 +5,11 @@ end
 
 #main function
 def caesar_cipher(str, shift_amount)
+
     alphabat = ("a".."z").to_a
     chars_of_str = str.chars
     finish_str = ""
+    
     chars_of_str.each do | char |
         if alphabat.any? { | letter_of_alphabat | letter_of_alphabat == char }  
             index_to_shift = alphabat.index(char)
@@ -31,26 +33,11 @@ def caesar_cipher(str, shift_amount)
             finish_str += char
         end
     end
+
     puts finish_str
 
-            
-    
-    ##letters_of_str.each do |letter|
-    #    if letter.to_i + shift_amount > 26
-    #        if is_upper?(letter)
-    #            new_str += alphabet[(letter.to_i + shift_amount) - 26 -2].to_upcase
-    #        else
-    #            new_str += alphabet[(letter.to_i + shift_amount) - 26 -2]
-    #        end
-    #    else 
-    #        if is_upper?(letter)
-    #            new_str += alphabet[letter.to_i + shift_amount].to_upcase
-    #        else
-    #            new_str += alphabet[letter.to_i + shift_amount]
-    #        end
-    #    end
-    #end
-    #puts new_str
 end
+
+
 puts "Enter a sentence than enter how many times you want it shifted: "
 caesar_cipher(gets.chomp, gets.chomp.to_i)
